d2668c1d7c4ee9e1fc30a658db1d2fe2cd349188
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-18 16:15:03 -0600

    clean comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,6 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :destroy, :edit]
   before_action :set_job, only: [:index, :new, :create, :edit, :update]
 
-  def index
-    @comments = @job.comments
-  end
-
   def new
     @comment = Comment.new
   end
@@ -18,32 +14,6 @@ class CommentsController < ApplicationController
 
     flash[:success] = "You said #{@comment.content} about #{@job.title}"
     redirect_to job_path(@comment.job)
-  end
-
-  def show
-    @comment = @article.comments.new
-  end
-
-  def edit
-  end
-
-  def update
-    @comment = @job.comments.find(params[:id])
-    @comment.update(comment_params)
-
-    if @job.save
-      flash[:success] = "You said #{@comment.content} about #{@job.title}"
-      redirect_to job_comment_path(@job, @comment)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @comment.destroy
-
-    flash[:success] = "#{@comment.content} was successfully deleted!"
-    redirect_to jobs_path
   end
 
   private
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,62 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :destroy, :edit]
+  before_action :set_company, only: [:index, :new, :create, :edit, :update]
+
+  def index
+    @contacts = @company.contacts
+  end
+
+  def new
+    @contact = Comment.new
+  end
+
+  def create
+    @contact = Comment.new(contact_params)
+    @contact.company_id = params[:company_id]
+
+    @contact.save
+
+    flash[:success] = "You said #{@contact.content} about #{@company.title}"
+    redirect_to company_path(@contact.company)
+  end
+
+  def show
+    @contact = @article.contacts.new
+  end
+
+  def edit
+  end
+
+  def update
+    @contact = @company.contacts.find(params[:id])
+    @contact.update(contact_params)
+
+    if @company.save
+      flash[:success] = "You said #{@contact.content} about #{@company.title}"
+      redirect_to company_contact_path(@company, @contact)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @contact.destroy
+
+    flash[:success] = "#{@contact.content} was successfully deleted!"
+    redirect_to companys_path
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:content)
+  end
+
+  def set_company
+    @company = Job.find(params[:company_id])
+  end
+
+  def set_contact
+    @contact = Comment.find(params[:id])
+  end
 end
