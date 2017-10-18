a09535bd56f5c6b4ec634e84df52dc0c1b25a5b1
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-18 15:00:40 -0600

    add controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,58 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :destroy, :edit]
+  before_action :set_job, only: [:index, :new, :create, :edit, :update]
+
+  def index
+    @comments = @job.comments
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @job.comments.create!(comment_params)
+
+    flash[:success] = "You said #{@comment.content} about #{@job.title}"
+    redirect_to job_comment_path(@job, @comment)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @comment = @job.comments.find(params[:id])
+    @comment.update(comment_params)
+
+    if @job.save
+      flash[:success] = "You said #{@comment.content} about #{@job.title}"
+      redirect_to job_comment_path(@job, @comment)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+
+    flash[:success] = "#{@comment.content} was successfully deleted!"
+    redirect_to jobs_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def set_job
+    @job = Job.find(params[:job_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 end
