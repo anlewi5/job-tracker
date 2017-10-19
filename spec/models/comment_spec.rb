7d357a6f6df546247f2105e067d14357d969c1df
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-18 23:38:39 -0600

    add comment validation tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without content" do
+        comment = build(:comment, content: "")
+        expect(comment).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with content and job" do
+        job = build(:job)
+        comment = build(:comment, job: job)
+        expect(comment).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "belongs to a job" do
+      comment = build(:comment)
+      expect(comment).to respond_to(:job)
+    end
+  end
 end
