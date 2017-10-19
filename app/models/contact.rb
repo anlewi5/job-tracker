991338ae5285a661bdef2fcf07fdbebbe9b698a4
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-18 23:02:22 -0600

    add validations to models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
+  validates :position,   presence: true
+  validates :email,      presence: true, uniqueness: true
+
   belongs_to :company
 end
